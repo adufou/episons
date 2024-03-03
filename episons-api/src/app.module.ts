7cebef1a108f01e63d8ba02c3fd92798c0d4ca91
Louis SANTUCCI <louissantucci1998@gmail.com>
2024-03-04 00:05:13 +0100

    [NESTJS] Created new modules

--- episons-api/src/app.module.ts
+++ episons-api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TracksModule } from './tracks/tracks.module';
+import { TagModule } from './tag/tag.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, TracksModule, TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
